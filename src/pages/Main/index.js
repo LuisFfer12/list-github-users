5877eac18634bee4fa366b9b6363f41cb2a30ecb
Luis Fernando Oliveira <luisfmoraes.r@gmail.com>
2019-09-18 20:22:52 -0300

    project created

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class Main extends Component {
+  state = {
+    loading: false,
+  };
+
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
